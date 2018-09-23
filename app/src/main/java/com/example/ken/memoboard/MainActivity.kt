3edcabe9e27eeafd9271577955a1983431a7ed37
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2018-09-23 14:07:38 +0900

    ボードの各画面への遷移(テスト用)

--- app/src/main/java/com/example/ken/memoboard/MainActivity.kt
+++ app/src/main/java/com/example/ken/memoboard/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.ken.memoboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import com.example.ken.memoboard.R.id.fab
+import com.example.ken.memoboard.R.id.toolbar
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +20,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+
+
+        boardActivity.setOnClickListener {
+            // 画面作成
+            val intent = Intent(this@MainActivity, BoardActivity::class.java)
+            // 画面遷移
+            startActivity(intent)
+        }
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
