58fa012a23f6dcbcb53d1b7c3024e52970769b16
skyloken <risora1234@gmail.com>
2018-09-29 21:56:14 +0900

    #23 色変更機能を実装

--- app/src/main/java/com/example/ken/memoboard/activity/MemoActivity.kt
+++ app/src/main/java/com/example/ken/memoboard/activity/MemoActivity.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
-import com.example.ken.memoboard.model.Memo
 import com.example.ken.memoboard.R
+import com.example.ken.memoboard.model.Memo
 import io.realm.Realm
 import io.realm.kotlin.where
 import kotlinx.android.synthetic.main.activity_memo.*
@@ -31,12 +31,17 @@ class MemoActivity : AppCompatActivity() {
         // テキストフィールドに表示
         editMemo.setText(memo?.text, TextView.BufferType.NORMAL)
 
+        // メモの色
+        val memoColor = intent.getIntExtra("memoColor", memo?.color!!)
+        editColorButton.setBackgroundColor(memoColor)
+
         // 保存ボタン
         saveButton.setOnClickListener {
 
             // save処理
             mRealm.executeTransaction {
                 memo?.text = editMemo.text.toString()
+                memo?.color = memoColor
             }
 
             val intent = Intent(this, BoardActivity::class.java)
@@ -69,5 +74,19 @@ class MemoActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+
+        // メモの色変更
+        editColorButton.setOnClickListener {
+
+            // 画面作成
+            val intent = Intent(this, MemoColorActivity::class.java)
+            intent.putExtra("memoId", memoId)
+            intent.putExtra("memoColor", memoColor)
+            // 画面遷移
+            startActivity(intent)
+
+        }
+
     }
+
 }
