f4596d1b5cacb56388b32505976b018ff9606e9f
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2018-10-15 01:35:48 +0900

    canvas描画

--- app/src/main/java/com/example/ken/memoboard/MyView.kt
+++ app/src/main/java/com/example/ken/memoboard/MyView.kt
@@ -10,8 +10,6 @@ import android.view.MotionEvent
 import android.view.View
 
 
-
-
 class MyView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private var pathList: ArrayList<Path>  // 直線リスト
@@ -19,16 +17,16 @@ class MyView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
 
     init {
-        //-- 初期化
-        //- Path関連
+        // 初期化
+        // Path関連
         pathList = ArrayList()   // リストの作成
 
-        //- Paint関連
+        // Paint関連
         paint = Paint()
         paint.color = Color.RED          // 色の指定
         paint.style = Paint.Style.STROKE // 描画設定を'線'に設定
-        paint.isAntiAlias = true           // アンチエイリアスの適応
-        paint.strokeWidth = 10f           // 線の太さ
+        paint.isAntiAlias = true         // アンチエイリアスの適応
+        paint.strokeWidth = 10f          // 線の太さ
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -44,19 +42,18 @@ class MyView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     override fun onTouchEvent(event: MotionEvent): Boolean {
 
         when (event.action) {
-            MotionEvent.ACTION_DOWN                     //- 画面をタッチしたとき
+            MotionEvent.ACTION_DOWN                     // 画面をタッチしたとき
             -> {
                 drawingPath = Path()                    // 新たなPathのインスタンスの作成
                 drawingPath!!.moveTo(event.x, event.y)  // 始点を設定
                 pathList.add(drawingPath!!)             // リストにPathを追加
             }
-            MotionEvent.ACTION_UP                       //- 画面から指を離したとき
+            MotionEvent.ACTION_UP                       // 画面から指を離したとき
             -> drawingPath!!.moveTo(event.x, event.y)   // 移動先の追加
-            MotionEvent.ACTION_MOVE                     //- タッチしながら指をスライドさせたとき
+            MotionEvent.ACTION_MOVE                     // タッチしながら指をスライドさせたとき
             -> drawingPath!!.lineTo(event.x, event.y)   // 移動先の追加
         }
-
-        return true   /* 返却値は必ず "true" にすること!! */
+        return true   // 返却値は必ずtrue
     }
 
 }
